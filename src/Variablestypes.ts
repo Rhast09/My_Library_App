9aacb099b57fa5cccfd6b9475147e5adac32bad5
Rhast09 <juanjarangog@gmail.com>
2024-06-13 07:31:54 -0500

    tipado primitivo

---
+++ src/Variablestypes.ts
@@ -0,0 +1,20 @@
+let bookTittle:string="TypeScript Basics"
+const libraryName:string="My Library"
+let numberofBooks:number=2006
+let islibraryOpen:boolean=true
+let titulo:string
+let autor:string
+let genero:string 
+let idioma:string
+let precio:number
+let formato:string 
+let isbn:number
+let descripcion:string
+let estado:string
+let ubicacion:string
+let fecha_publicacion:Date
+let eitorial:string
+let paginas:string
+let dimensiones:string
+let peso:string
+let disponible:boolean
